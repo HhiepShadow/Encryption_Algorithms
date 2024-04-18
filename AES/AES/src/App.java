d74c6e9047265f17aa6c15b2706465683faa17ef
HHiepShadow <hth1292003@gmail.com>
2024-04-18 17:44:43 +0700

    Change AES

--- AES/AES/src/App.java
+++ AES/AES/src/App.java
@@ -2,8 +2,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         String key = "021D3D04A490B5A4C91A4F85112A5B55";
         String m = "7BB88955B6E87E91095C2A880F983F46";
-        AES aes = new AES(key.getBytes());
-        byte[] cipher = aes.ECB_encrypt(m.getBytes());
-        System.out.println(cipher);
+        AES_128 aes = new AES_128(key);
+        int[][] C = aes.encrypt(m);
+        for (int[] s : C) {
+            for (int c : s) {
+                if (Integer.toHexString(c).length() == 1)
+                    System.out.print("0" + Integer.toHexString(c));
+                else
+                    System.out.print(Integer.toHexString(c));
+            }
+        }
     }
 }
